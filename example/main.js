2baa161529830f8710ea028b9884cf4fc3c7c0d3
thesoundfromthesky <3rdworldstrawberry@gmail.com>
2020-06-24 17:04:05 -0500

    Get bare minimum example. Implement basic control buttons(start, stop pause/resume). Show current time.

---
+++ example/main.js
@@ -0,0 +1,58 @@
+const timer = init_timer();
+
+const [container] = document.getElementsByClassName("container");
+const [display] = document.getElementsByClassName("display");
+const pause = document.querySelector(".control .pause");
+
+function init_timer() {
+  const timer = new Timer({
+    on_start,
+    on_end,
+    on_update,
+  });
+
+  return (() => {
+    return timer;
+  })();
+}
+
+const STATUS = { RUNNING: "running", STOPPED: "stopped", PAUSED: "paused" };
+function remove_all_status() {
+  container.classList.remove(...Object.values(STATUS));
+}
+
+function set_status(status) {
+  remove_all_status();
+  container.classList.add(status);
+}
+
+function on_start() {
+  set_status(STATUS.RUNNING);
+}
+
+function on_end() {
+  set_status(STATUS.STOPPED);
+  display.textContent = "0";
+  pause.onclick = on_pause;
+  pause.textContent = "pause";
+}
+
+function on_pause() {
+  if (timer.is_running && !timer.paused_time) {
+    set_status(STATUS.PAUSED);
+    pause.onclick = on_resume;
+    timer.pause();
+    pause.textContent = "resume";
+  }
+}
+
+function on_resume() {
+  set_status(STATUS.RUNNING);
+  pause.onclick = on_pause;
+  timer.resume();
+  pause.textContent = "pause";
+}
+
+function on_update() {
+  display.textContent = timer.get_time();
+}
